2e78a071d19f5da0d5cb872ab75fd4eaa6e36f65
Lorena-Pinheiro <107314914+Lorena-Pinheiro@users.noreply.github.com>
2024-10-08 12:05:41 -0300

    Backend: ok, Frontend: not ok

--- webpageFuncionario/index.php
+++ webpageFuncionario/index.php
@@ -3,34 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <form method="get">
-        <label for="nome">Nome: </label>
-        <input type="text" name="nome" id="nome">
 
-        <label for="salario">Salário: </label>
-        <input type="number" name="salario" id="salario">
+    <header class="header-site">
 
-        <select name="cargo" id="cargo">
-            <option value="Gerente">Gerente</option>
-            <option value="Analista de TI">Analista de TI</option>
-            <option value="Desenvolvedora">Desenvolvedora</option>
-            <option value="Designer">Designer</option>
-            <option value="Assistente Administrativo">Assistente Administrativo</option>
-            <option value="Consultor">Consultor</option>
-        </select>
+        <a href="#" class="img">
+            <img src="https://indy-systems.imgix.net/tsa14o3kh1lt3ts7b8vwz6m3atbk?max-w=1000" alt="logo" style="height: 130px;">
+        </a>    
+
+        <ul class="header-link-list">
+            <li class="header-link">
+                <a href="#">Início</a>
+            </li>
+            <li class="header-link">
+                <a href="#">Sobre</a>
+            </li>
+            <li class="header-link">
+                <a href="#">Suporte</a>
+            </li>
+        </ul>
         
+    </header>
 
-        <label for="idade">Idade: </label>
-        <input type="number" name="idade" id="idade">
+    
+    <section class="cadastro">
+        <h1>Cadastro de Funcionário</h1>
+        <form method="get" class="form-cadastro">
+            <input type="text" name="nome" id="nome" placeholder="Nome: " class="input-form-cadastro">
 
-        <label for="tel">Telefone: </label>
-        <input type="text" name="tel" id="tel">
+            <input type="number" name="salario" id="salario" placeholder="Salário: " class="input-form-cadastro">
 
-        <input type="submit" value="Cadastrar">
-    </form>
+            <select name="cargo" id="cargo">
+                <option value="Gerente">Gerente</option>
+                <option value="Analista de TI">Analista de TI</option>
+                <option value="Desenvolvedora">Desenvolvedora</option>
+                <option value="Designer">Designer</option>
+                <option value="Assistente Administrativo">Assistente Administrativo</option>
+                <option value="Consultor">Consultor</option>
+            </select>
+            
+            <input type="number" name="idade" id="idade" placeholder="Idade: " class="input-form-cadastro">
+
+            <input type="text" name="tel" id="tel" placeholder="Telefone: " class="input-form-cadastro">
+
+            <input type="submit" value="Cadastrar">
+        </form>
+    </section>
+
+
+    <section class="filter">
+        <form method="get" class="form-filter">
+            <select name="cargo-filter" id="cargo-filter">
+                <option value="Selecionar">-- Selecionar --</option>
+                <option value="Gerente">Gerente</option>
+                <option value="Analista de TI">Analista de TI</option>
+                <option value="Desenvolvedora">Desenvolvedora</option>
+                <option value="Designer">Designer</option>
+                <option value="Assistente Administrativo">Assistente Administrativo</option>
+                <option value="Consultor">Consultor</option>
+            </select>
+
+            <input type="submit" value="Filtrar">
+        </form>
+    </section>
 
     <?php
         $servidor = 'localhost';
@@ -39,16 +77,42 @@
         $banco_de_dados = 'empresa';
         $conexao = mysqli_connect($servidor,$usuario,$senha,$banco_de_dados);
 
-        $nome = $_GET['nome']; 
-        $salario = $_GET['salario'];
-        $cargo = $_GET['cargo'];
-        $idade = $_GET['idade'];
-        $tel = $_GET['tel'];
+        if(isset($_GET['nome'], $_GET['salario'], $_GET['cargo'], $_GET['idade'], $_GET['tel'])) {
+            $nome = $_GET['nome']; 
+            $salario = $_GET['salario'];
+            $cargo = $_GET['cargo'];
+            $idade = $_GET['idade'];    
+            $tel = $_GET['tel'];
 
-        $conexao->query('insert into funcionario (nome, cargo, idade, tel, salario) values ('$nome', '$cargo', $idade, '$tel', $salario)');
-    
-    
+            $conexao->query("insert into funcionario (nome, cargo, idade, tel, salario) values ('$nome', '$cargo', '$idade', '$tel', '$salario')");
+            $conexao->close();
+        }
+
+        
+        if(isset($_GET['cargo-filter']) && $_GET['cargo-filter'] != "Selecionar" ){
+            $cargoFilter = $_GET['cargo-filter'];
+            $selectFuncionarios = $conexao->query("select * from funcionario where cargo = '$cargoFilter'");
+            $rowFuncionario = $selectFuncionarios->fetch_all(MYSQLI_ASSOC);
+            foreach ($rowFuncionario as $funcionario){
+                include 'template.php';
+            };
+        }else if(!isset($_GET['cargo-filter']) || ($_GET['cargo-filter'] == "Selecionar")){
+            $selectFuncionarios = $conexao->query('select * from funcionario');
+            $rowFuncionario = $selectFuncionarios->fetch_all(MYSQLI_ASSOC);
+            foreach ($rowFuncionario as $funcionario){
+                include 'template.php';
+            };
+        }
     ?>
+    
+
+    <footer class="footer-site">
+        <a href="#" class="img">
+            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c45c69b2-249c-40d1-9981-6c7ade872a9a/dhbd04u-48418a16-5a7c-450d-8eb1-339ae02fc660.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2M0NWM2OWIyLTI0OWMtNDBkMS05OTgxLTZjN2FkZTg3MmE5YVwvZGhiZDA0dS00ODQxOGExNi01YTdjLTQ1MGQtOGViMS0zMzlhZTAyZmM2NjAuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.bRyLL2GHKUjx8OjEKWJlPecsWWlcqYhve6RYE58HwDo" alt="logo">
+        </a>
+    </footer>
+
+    
 
 </body>
 </html>
--- webpageFuncionario/template.php
+++ webpageFuncionario/template.php
@@ -1,10 +1,10 @@
 <div class="card-funcionario">
-    <header>
+    <header class="header-card">
         <h2>Nome: <?= $funcionario['nome']?></h2>
         <p>Cargo: <?= $funcionario['cargo']?></p>
         <p>Idade: <?= $funcionario['idade']?></p>
     </header>
-    <footer>
+    <footer class="footer-card">
         <a href="tel:<?= $funcionario['tel']?>">Telefone: <?= $funcionario['tel']?></a>
         <span>ID: <?= $funcionario['id_funcionario']?></span>
     </footer>
